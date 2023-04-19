6f6bd86b84b215cb693cf805ca5ebf64c25a8088
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-04-19 11:01:08 -0500

    auto save

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<nav id="navBar" class="flex">
+	<ul class="flex items-center justify-around w-screen h-20">
+		<li><a href="/">How it works</a></li>
+		<li><a href="/">Pricing</a></li>
+		<li><a href="/">Services</a></li>
+		<button class="" id="login">Login</button>
+		<button id="register">Register</button>
+	</ul>
+</nav>
+<style>
+	#navBar {
+	background-color: var(--primary-color);
+}
+
+</style>
