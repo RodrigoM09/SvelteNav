1349017fedd37091b016d4e60df8307fd1feab37
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-04-19 12:19:58 -0500

    auto save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,26 @@
+<script>
+  import "../app.css";
+  import SonixColourLog from "../lib/images/Sonix-colour-logo-02(1).svg";
+
+</script>
+
 <nav id="navBar" class="flex justify-between items-center w-screen">
-	<div class="m-5" id="logo">
-		<img src="https://picsum.photos/100/50" alt="">
-	</div>
+	
+		<img class="w-1/6" src={SonixColourLog} alt="">
+	
 	<div class="flex w-6/12">
-		<ul class="flex items-center justify-between w-full h-20 text-white">
-			<li><a href="/">How it works</a></li>
+		<ul class="flex items-center gap-20 h-20 text-white">
+			<li><a href="/">How it Works</a></li>
 			<li><a href="/">Pricing</a></li>
 			<li><a href="/">Services</a></li>
 		</ul>
-		<div class="flex items-center justify-between m-5 gap-1" id="navButtons">
-			<button class="bg-white rounded-lg w-20 h-7" id="login">Login</button>
-			<button class="rounded-lg w-20 h-7" id="register">Register</button>
+		<div class="flex items-center justify-between m-5 gap-5" id="navButtons">
+			<button class="bg-white rounded-xl w-20 h-7" id="login">Login</button>
+			<button class="rounded-xl w-20 h-7" id="register">Register</button>
 		</div>
 	</div>
 </nav>
-
 <slot />
-
 <style>
 	#navBar {
 	background-color: var(--primary-color);
@@ -26,4 +30,3 @@
 	}
 
 </style>
-
